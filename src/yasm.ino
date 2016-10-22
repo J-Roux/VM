02f1f790aac113826efb70940e2565bcb95727ad
J-Roux <aps7@tut.by>
2016-10-22 17:07:06 +0300

    add travis

---
+++ src/yasm.ino
@@ -0,0 +1,12 @@
+#include "vm.h"
+
+void setup() {
+  uint8_t *code;
+  uint16_t programm_counter;
+  execute_step(code,&programm_counter);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
